ff0d29438779437979ab9add1ac6008ec5afaecf
ko3luhbka <ko3luhbka@gmail.com>
2020-03-15 12:07:02 +0300

    Move production config to separate module; use .env file for storing sensitive data

---
+++ flaskr/config.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config:
+
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
