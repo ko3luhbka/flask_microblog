8af5f1c6d22aab6eb6086690fa00a02629a0a0bc
ko3luhbka <ko3luhbka@gmail.com>
2020-03-15 19:12:32 +0300

    Set default log level to DEBUG

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -47,9 +47,9 @@ def create_app(test_config=None):
         file_handler.setFormatter(logging.Formatter(
             '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
         )
-        file_handler.setLevel(logging.INFO)
+        file_handler.setLevel(logging.DEBUG)
         app.logger.addHandler(file_handler)
 
-        app.logger.setLevel(logging.INFO)
+        app.logger.setLevel(logging.DEBUG)
         app.logger.info('Flaskr startup')
     return app
