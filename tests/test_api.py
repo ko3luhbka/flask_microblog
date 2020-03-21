ceae7d65ad3e70bcc1da6d5fa8ca47a2d5fd3764
ko3luhbka <ko3luhbka@gmail.com>
2020-03-21 18:12:34 +0300

    Add test for users update API endpoint

--- tests/test_api.py
+++ tests/test_api.py
@@ -31,7 +31,7 @@ def test_get_users(app, client):
         'first_name': 'NewUser1FirstName',
         'last_name': 'NewUser1LastName',
         'username': 'new_user1',
-        'password': 'pass1'
+        'password': 'pass1',
     },
     {
         'username': 'new_user2',
@@ -57,7 +57,7 @@ def test_create_user_existing(app, client):
         existing_user = User.query.first()
     response = client.post('/api/users', json={
         'username': existing_user.username,
-        'password': 'supersecret'
+        'password': 'supersecret',
     })
     assert response.status_code == 400
     response_data = response.get_json()
@@ -69,7 +69,7 @@ def test_create_user_existing(app, client):
     {
         'first_name': 'NewUser1FirstName',
         'last_name': 'NewUser1LastName',
-        'password': 'pass1'
+        'password': 'pass1',
     },
     {
         'first_name': 'NewUser2FirstName',
@@ -84,3 +84,49 @@ def test_create_user_bad_fields(app, client, user_data):
         'Username and password are mandatory fields'
     with app.app_context():
         assert User.query.filter_by(first_name=user_data['first_name']).first() is None
+
+
+@pytest.mark.parametrize('user_id, user_data, status_code, err_msg', (
+    (1,
+    {
+        'id': 666,
+        'first_name': 'NewUser2FirstName',
+        'username': 'other',
+    },
+    400,
+    'Please use a different username'),
+    (1,
+    {
+        'id': 666,
+        'first_name': 'ChangedFirstName',
+        'last_name': 'ChangedLastName',
+        'password': 'changed_pass',
+        'username': 'new_username',
+    },
+    200,
+    None),
+    (1,
+    {
+        'id': 1,
+        'first_name': 'ChangedFirstName2',
+        'last_name': 'ChangedLastName2',
+        'password': 'changed_pass2',
+        'username': 'test',
+    },
+    200,
+    None),
+))
+def test_update_user(app, client, user_id, user_data, status_code, err_msg):
+    response = client.put('/api/users/{}'.format(user_id), json=user_data)
+    assert response.status_code == status_code
+    response_data = response.get_json()
+    assert response_data.get('message') == err_msg
+    if response.status_code != 200:
+        return
+    with app.app_context():
+        db_user = User.query.get(user_id)
+        for key, value in user_data.items():
+            if key in ('id', 'password'):
+                continue
+            assert response_data[key] == value
+            assert getattr(db_user, key) == value
